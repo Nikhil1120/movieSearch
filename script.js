7f8cbb7a077e9439cd09a59cf5180543e77f02e6
Nikhil1120 <NIKHILT1120@GMAIL.COM>
2024-09-07 14:05:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const mainDiv = document.getElementById('main');
+const input = document.getElementById('inp')
+
+const fetchData = () => {
+    fetch(`https://www.omdbapi.com/?s=${input.value}&apikey=3429085f`) // fetch will return a promise
+        .then((res) => res.json())// promises resolves and returns another promise for converting json
+        .then((data) => displayData(data.Search))
+        .catch((err) => console.log(err))
+}
+
+document.getElementById("search").addEventListener("click", fetchData)
+
+const displayData = (arr) => {
+    mainDiv.textContent = '';
+    arr.forEach((ele) => {
+        const movieDiv = document.createElement('div');
+        movieDiv.classList.add('movie');
+
+        const movieImg = document.createElement('img')
+        movieImg.src = ele.Poster;
+        movieImg.alt = ele.Title;
+
+        const movieTitle = document.createElement('h2')
+        movieTitle.textContent = ele.Title;
+
+        const movieYear = document.createElement('p');
+        movieYear.textContent = "YEAR: " + ele.Year
+
+        movieDiv.append(movieImg, movieTitle, movieYear)
+        mainDiv.append(movieDiv)
+    })
+}
+
+   
